07c528ad61abd30a7cd22fa628c626ca2b85087a
RichardMEN11 <richardmenning@googlemail.com>
2023-05-18 21:22:50 +0200

    feat: updated design

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,161 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client';
+import {
+  Avatar,
+  Box,
+  Button,
+  Center,
+  Circle,
+  Flex,
+  FormControl,
+  Grid,
+  HStack,
+  Heading,
+  Input,
+  Spacer,
+  VStack,
+  Text,
+} from '@chakra-ui/react';
+import Image from 'next/image';
+import { useState } from 'react';
+
+const pre = [
+  {
+    user: 'bot',
+    message: 'Hello 👋, my name is Her Flourish. I am your financial friend.',
+  },
+  {
+    user: 'bot',
+    message: 'How can I help you today?',
+  },
+];
 
 export default function Home() {
+  const [messages, setMessages] = useState(pre);
+  const [newMessage, setNewMessage] = useState('');
+
+  const handleNewMessage = (e: any) => {
+    e.preventDefault();
+    if (newMessage) {
+      setMessages([...messages, { user: 'user', message: newMessage }]);
+      setNewMessage('');
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Grid
+      width="100vw"
+      height="100vh"
+      gridTemplateRows="auto 50px 70px"
+      background="linear-gradient(184deg, rgba(64,82,190,1) 0%, rgba(219,70,125,1) 84%)"
+      position="relative"
+    >
+      <HStack
+        position="absolute"
+        top="0"
+        w="100%"
+        p="20px"
+        alignItems="center"
+        gap="10px"
+        zIndex="1000"
+      >
+        <Avatar
+          size="md"
+          mt="auto"
+          src="https://i.pinimg.com/550x/50/b2/6c/50b26ce6d2bc8adcf0e9a720c6fdc3f9.jpg"
+          border="4px solid #41C3A7"
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+        <VStack align="left" spacing="2px">
+          <Heading size="sm" color="#fff">
+            Your virtual assistant
+          </Heading>
+          <HStack>
+            <Circle
+              backgroundColor="#3EE923"
+              size="2"
+              border="2px solid white"
+            />
+            <Text color="rgba(217, 217, 217, 0.8);" fontSize="12px">
+              online now & always
+            </Text>
+          </HStack>
+        </VStack>
+      </HStack>
+      <Box
+        bgColor="rgba(217, 217, 217, 0.1);"
+        paddingTop="100px"
+        paddingX="10px"
+        overflow="scroll"
+        gap="15px"
+        display="flex"
+        flexDirection="column"
+      >
+        {messages.map((data: any) => {
+          return (
+            <Flex
+              key={data.message}
+              justifyContent={data.user === 'bot' ? 'flex-start' : 'flex-end'}
+              w="100%"
+              gap="10px"
+            >
+              {data.user === 'bot' && (
+                <Avatar
+                  size="xs"
+                  mt="auto"
+                  src="https://i.pinimg.com/550x/50/b2/6c/50b26ce6d2bc8adcf0e9a720c6fdc3f9.jpg"
+                />
+              )}
+              <Box
+                backgroundColor={data.user === 'bot' ? 'gray.100' : 'gray.400'}
+                padding="12px"
+                maxW="60%"
+                borderRadius="20px"
+              >
+                {data.message}
+              </Box>
+              {data.user !== 'bot' && <Avatar size="xs" mt="auto" />}
+            </Flex>
+          );
+        })}
+      </Box>
+      <HStack p="5px 15px" backgroundColor="#fff">
+        <form onSubmit={(e) => handleNewMessage(e)} style={{ width: '100%' }}>
+          <FormControl
+            display="grid"
+            gridTemplateColumns="auto 40px"
+            gap="10px"
+          >
+            <Input
+              placeholder="Ask a question"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+            />
+            <Button type="submit" backgroundColor="gray.100">
+              <Image src="/assets/send.png" alt="icon" width="50" height="50" />
+            </Button>
+          </FormControl>
+        </form>
+      </HStack>
+      <HStack
+        bgColor="#FFFFFF"
+        px="25px"
+        py="10px"
+        borderTop="1px solid lightgray"
+        align="center"
+        justifyContent="space-between"
+      >
+        <Image src="/assets/icon1.png" alt="icon" width="30" height="30" />
+        <Image src="/assets/icon2.png" alt="icon" width="30" height="30" />
+        <Circle
+          size="50px"
+          backgroundColor="#DF3161"
+          fontSize="24px"
+          color="#fff"
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+          ?
+        </Circle>
+        <Image src="/assets/icon3.png" alt="icon" width="30" height="30" />
+        <Image src="/assets/icon4.png" alt="icon" width="30" height="30" />
+      </HStack>
+    </Grid>
+  );
 }
