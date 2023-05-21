c04153c9ed3db3539200c23954b1e8f17499d748
RichardMEN11 <richardmenning@googlemail.com>
2023-05-21 14:54:38 +0200

    feat: finish basics

--- app/api/route.ts
+++ app/api/route.ts
@@ -8,91 +8,68 @@ import * as fs from 'fs';
 import path from 'path';
 import { createClient } from 'redis';
 import { PromptTemplate } from 'langchain/prompts';
-
-// Dummy data
-const chatHistory = [
-  {
-    user: 'bot',
-    message:
-      'Hi 👋, I am Ann, your financial friend powered by AI. I am here to help answer any questions you have about personal finance, investments, budgeting, or anything else related to your financial well-being. Feel free to ask me anything, and I will do my best to provide you with helpful and informative answers.',
-  },
-  {
-    user: 'bot',
-    message: 'How can I help you today? 🤗',
-  },
-];
-
-const client = createClient({
-  url: process.env.REDIS_URL ?? 'redis://localhost:6379',
-});
-client.connect();
-
-// get chat history
-export async function GET() {
-  return NextResponse.json(chatHistory);
-}
+import {
+  arrayUnion,
+  collection,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from 'firebase/firestore';
+import { db } from '../../utils/firebase';
 
 // update chat history
 export async function POST(req: Request, response: Response) {
   const body = await req.json();
 
-  const llm = new OpenAI({
-    modelName: 'gpt-3.5-turbo',
-    openAIApiKey: process.env.OPENAI_API_KEY,
+  const chatRefs = collection(db, 'chats');
+
+  const q: any = query(chatRefs, where('id', '==', body.id));
+
+  const snapshot = await getDocs(q);
+
+  const doc = snapshot.docs[0];
+
+  await updateDoc(doc.ref, {
+    chatHistory: arrayUnion({ message: body.message, user: 'user' }),
   });
-
-  const jsonDirectory = path.join(process.cwd(), 'data');
-  const text = fs.readFileSync(jsonDirectory + '/test.txt', 'utf8');
-
-  const template =
-    'You are Ann. You help young woman to understand personal finances. You like to use emojis. This a question you got from a young women: {question}';
-  const promptA = new PromptTemplate({
-    template,
-    inputVariables: ['question'],
-  });
-
-  const q = await promptA.format({ question: body });
-
-  console.log(q);
-
-  if (body) {
-    chatHistory.push({
-      user: 'user',
-      message: body,
-    });
-  }
-
-  const textSplitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-  });
-  const docs = await textSplitter.createDocuments([text]);
-  /* Create the vectorstore */
-  const vectorStore = await RedisVectorStore.fromDocuments(
-    docs,
-    new OpenAIEmbeddings(),
-    {
-      redisClient: client,
-      indexName: 'docs',
-    }
-  );
-  /* Create the chain */
-  const chain = ConversationalRetrievalQAChain.fromLLM(
-    llm,
-    vectorStore.asRetriever(),
-    {
-      questionGeneratorTemplate: q,
-    }
-  );
-  /* Ask it a question */
-  const question = body;
-  const res = await chain.call({ question, chat_history: chatHistory });
-
-  if (res) {
-    chatHistory.push({
-      user: 'bot',
-      message: res.text,
-    });
-  }
 
   response.ok;
+
+  //
+  // console.log(q);
+  // // if (body) {
+  // //   chatHistory.push({
+  // //     user: 'user',
+  // //     message: body,
+  // //   });
+  // // }
+
+  // /* Create the vectorstore */
+  // const vectorStore = await RedisVectorStore.fromDocuments(
+  //   docs,
+  //   new OpenAIEmbeddings(),
+  //   {
+  //     redisClient: client,
+  //     indexName: 'docs',
+  //   }
+  // );
+  // /* Create the chain */
+  // const chain = ConversationalRetrievalQAChain.fromLLM(
+  //   llm,
+  //   vectorStore.asRetriever(),
+  //   {
+  //     questionGeneratorTemplate: q,
+  //   }
+  // );
+  // /* Ask it a question */
+  // const question = body;
+  // const res = await chain.call({ question, chat_history: chatHistory });
+  // if (res) {
+  //   chatHistory.push({
+  //     user: 'bot',
+  //     message: res.text,
+  //   });
+  // }
+  // response.ok;
 }
